f4ebc889d1b50d8c8214db015ac7f706449b48c5
Olesh <olegkirashchuk@gmail.com>
2019-12-03 18:39:15 +0200

    fixed .env problem; (.env file must be created in backend root)

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,9 +1,9 @@
 import Sequelize from 'sequelize'
 
-const database = process.env.database || 'database'
-const username = process.env.username || 'root'
-const password = process.env.password || ''
-const dbhost = process.env.dbhost || 'localhost'
+const database = process.env.DATABASE_NAME || 'database'
+const username = process.env.DB_USERNAME || 'root'
+const password = process.env.DB_PASSWORD || ''
+const dbhost = process.env.DB_HOST || 'localhost'
 
 const sequelize = new Sequelize(database, username, password, {
   host: dbhost,
