136ec9fdcbb060d98af379058f476ef229792775
olesh <olegkirashchuk@gmail.com>
2019-12-07 02:03:47 +0200

    update subject
    updated specialty, subjects, subjectTypes to check whether such exists when creating new items

--- backend/src/modules/subjectType/controller.js
+++ backend/src/modules/subjectType/controller.js
@@ -2,18 +2,37 @@ import SubjectType from '../../models/subject_types'
 
 export default class SubjectTypeController {
   static async createSubjectType({ name, coefficient }) {
-    const create = await SubjectType.create({
+    const subjectTypeData = {
       name,
       coefficient,
-    })
-
-    if (!create.dataValues) {
-      return { created: false }
     }
 
-    return {
-      created: true,
-      subjectType: create.dataValues,
+    try {
+      const exists = await SubjectType.findAll({
+        where: subjectTypeData,
+      })
+
+      if (exists.length) {
+        return {
+          created: false,
+          msg: 'Such subject type already exist',
+          param: 'name',
+          location: 'body',
+        }
+      }
+
+      const create = await SubjectType.create(subjectTypeData)
+
+      if (!create.dataValues) {
+        return { created: false }
+      }
+
+      return {
+        created: true,
+        subjectType: create.dataValues,
+      }
+    } catch (e) {
+      return { created: false }
     }
   }
 
