665d337ada63b61360a474b434f33b415993182c
olesh <olegkirashchuk@gmail.com>
2019-12-11 04:30:46 +0200

    update classes

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import hours from './modules/hours'
 import commissions from './modules/commissions'
 import department from './modules/department'
 import semester from './modules/semester'
+import classes from './modules/classes'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -35,5 +36,6 @@ hours(app)
 commissions(app)
 department(app)
 semester(app)
+classes(app)
 
 app.listen(port)
