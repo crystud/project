599d8a77c8f1d492f355073344ac7f2baaad4ece
Andrei Grigortsev <grigortsevandrei@gmail.com>
2019-12-16 09:31:10 +0200

    clear client tools/accessTokenHeader.js

--- frontend/src/tools/accessTokenHeader.js
+++ frontend/src/tools/accessTokenHeader.js
@@ -1,7 +1,9 @@
 export default (axiosInstance, store) => {
   axiosInstance.interceptors.request.use(async (request) => {
-    // eslint-disable-next-line no-param-reassign
-    request.headers.authorization = `Bearer ${store.getters['authorization/refreshToken']}`
+    if (store.getters['authorization/authorized']) {
+      // eslint-disable-next-line no-param-reassign
+      request.headers.authorization = `Bearer ${store.getters['authorization/refreshToken']}`
+    }
 
     return request
   })
